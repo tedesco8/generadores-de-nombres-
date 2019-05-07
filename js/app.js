a9869684f5196cb8cf62581ab5694593f52c71dc
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-05-06 22:34:40 -0300

    Enviando la peticion AJAX, leyendo e imprimiendo los resultados

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.querySelector('#generar-nombre').addEventListener('submit', cargarNombr
 //Llamado a Ajax e imprimir resultados
 function cargarNombres(e){
 e.preventDefault();
+
 //Leer las variables
 const origen = document.getElementById('origen');
 const origenSeleccionado = origen.options[origen.selectedIndex].value;
@@ -14,6 +15,7 @@ const cantidad = document.getElementById('numero').value;
 
 let url = '';
 url += 'https://uinames.com/api/?';
+
 //Si hay origen agregarlo a la URL
 if(origenSeleccionado !== ''){
     url += `region=${origenSeleccionado}&`;
@@ -26,5 +28,34 @@ if(generoSeleccionado !== ''){
 if(cantidad !== ''){
     url += `amount=${cantidad}&`;
 }
-console.log(url);
+
+//Conectar con AJAX
+
+//Iniciar XMLHTTPRequest
+const xhr = new XMLHttpRequest();
+//Abrimos la conexion
+xhr.open('GET', url, true);
+//Datos e impresion del template
+xhr.onload = function() {
+    if(this.status === 200){
+        const nombres = JSON.parse(this.responseText);
+        //Generar Template
+        let htmlNombres = '<h2>Nombres Generados</h2>';
+
+        htmlNombres += '<ul class="lista">';
+        nombres.forEach(function(nombre){
+            htmlNombres += `
+            <li>${nombre.name}        
+            `;
+        })
+        
+        htmlNombres += '</ul>'
+        document.getElementById('resultado').innerHTML = htmlNombres;
+    }
 }
+
+//Enviar el Request
+xhr.send();
+
+}
+
